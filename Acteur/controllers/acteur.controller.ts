095f84a5997a5666984762ea9a57d8f718240495
Sins-Luxuria <pmatteo032002@gmail.com>
2025-03-04 13:25:10 +0100

    update mongodb + gestion d'erreurs amélioré

--- Acteur/controllers/acteur.controller.ts
+++ Acteur/controllers/acteur.controller.ts
@@ -2,18 +2,46 @@ import * as service from '../services/acteur.service.ts';
 import { Acteur } from '../models/acteur.model.ts';
 import { RouterContext } from "https://deno.land/x/oak@v17.1.4/mod.ts";
 
-export const getAllActeurs = (ctx: RouterContext<'/acteurs'>)=> {
-    ctx.response.body = service.getAllActeurs
+export const getAllActeurs = (ctx: RouterContext<'/acteurs'>) => {
+    try { ctx.response.body = service.getAllActeurs }
+    catch (error) {
+        ctx.response.status = 404;
+        ctx.response.body = (error as Error).message;
+    }
 }
 export const getActeurById = (ctx: RouterContext<'/acteurs/:id'>) => {
-    ctx.response.body = service.getActeurById(parseInt(ctx.params.id));
+    try {
+        ctx.response.body = service.getActeurById(ctx.params.id);
+    } catch (error) {
+        ctx.response.status = 404
+        ctx.response.body = (error as Error).message;
+    }
 }
 export const createActeur = async (ctx: RouterContext<'/acteurs'>) => {
-    ctx.response.body = service.createActeur(await ctx.request.body.json());
+    
+    try {
+        service.createActeur(await ctx.request.body.json())
+        ctx.response.body = { message: "Acteur créé" };
+    } catch (error) {
+        ctx.response.status = 404
+        ctx.response.body = (error as Error).message;
+    }
 }
 export const updateActeur = async (ctx: RouterContext<'/acteurs/:id'>) => {
-    ctx.response.body = service.updateActeur(parseInt(ctx.params.id), await ctx.request.body.json())
+    try {
+        service.updateActeur(ctx.params.id, await ctx.request.body.json())
+        ctx.response.body = { message: "Acteur modifié" };
+    } catch (error) {
+        ctx.response.status = 404
+        ctx.response.body = (error as Error).message;
+    }
 }
 export const deleteActeur = async (ctx: RouterContext<'/acteurs/:id'>) => {
-    ctx.response.body = service.deleteActeur(parseInt(ctx.params.id));
+    try {
+        service.deleteActeur(ctx.params.id);
+        ctx.response.body = { message: "Acteur supprimé" };
+    } catch (error) {
+        ctx.response.status = 404
+        ctx.response.body = (error as Error).message;
+    }
 }
---
+++ Acteur/repositories/db.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from "https://deno.land/x/mongo@v0.31.2/mod.ts";
+
+const MONGODB_URI = "mongodb://localhost:27017";
+const DB_NAME = "td2-deno-mongodb";
+
+if (!MONGODB_URI) {
+    console.error("❌ MONGO_URI is not set");
+}
+
+let client: MongoClient;
+let db: any;
+let acteurs;
+
+try {
+    console.log("⏳ Connexion à MongoDB...");
+    client = new MongoClient();
+    await client.connect(MONGODB_URI);
+    db = client.database(DB_NAME);
+    console.log("✅ Connecté à MongoDB");
+    acteurs = db.collection("acteurs");
+    console.log("acteurs = "+ acteurs)
+} catch (error) {
+    console.error("❌ Erreur de connexion MongoDB:", error);
+}
+
+export { db, acteurs };
